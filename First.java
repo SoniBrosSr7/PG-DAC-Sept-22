ec2bb0d98bec0fb3feb113c3198cc62a2685384e
TARUN KUMAR SONI <108720164+SoniBrosSr7@users.noreply.github.com>
2022-09-13 16:47:12 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,6 @@
+public class First{}
+
+//F:\Java Practice\09-09-22>java First
+//Error: Main method not found in class First, please define the main method as:
+//   public static void main(String[] args)
+//or a JavaFX application class must extend javafx.application.Application
